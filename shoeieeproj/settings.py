ad894a3eba5b87ed6a163254e14dc2815325fbe5
Rahul AR <rahulrggr@gmail.com>
2022-10-15 16:37:55 +0530

    tk

--- shoeieeproj/settings.py
+++ shoeieeproj/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from django.utils import timezone
-import datetime
 from decouple import config 
 # import sentry_sdk
 # from sentry_sdk.integrations.django import DjangoIntegration
